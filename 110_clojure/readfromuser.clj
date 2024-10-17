374326a5d96a7e21ff1629c25871317c8d788810
MuggleBornPadawan <mugglebornpadawan@icloud.com>
2024-10-17 17:41:24 +0530

    Update readfromuser.clj

--- 110_clojure/readfromuser.clj
+++ 110_clojure/readfromuser.clj
@@ -1,4 +1,5 @@
 (defn echo-uppercase []
   (println "Enter a string:")
   (let [input (read-line)]
-    (println (clojure.string/upper-case input))))
+    (println "given string" input)
+    (println "output string" (clojure.string/upper-case input))))
