403041052ca5329d6fbb08273f0ec28818f5ef8c
MuggleBornPadawan <mugglebornpadawan@icloud.com>
2025-03-21 17:19:13 +0530

    daily

--- 700_linux/remote_startup.sh
+++ 700_linux/remote_startup.sh
@@ -1,6 +1,7 @@
 # ./MuggleBornPadawan/700_linux/remote_startup.sh | tee - a ./MuggleBornPadawan/700_linux/bckp/shell_log.log
+# ./MuggleBornPadawan/700_linux/bckp/bckp.sh
 clear
-echo "use this for any remote debian server setup and startup"
+echo "use this for any remote debian server setup, startup and chk backups"
 echo -e "\nDate: $(date) \nOS: $(uname -s) \nKernel: $(uname -r)"
 sudo apt-get update
 sudo apt-get dist-upgrade
@@ -12,6 +13,9 @@ alias rm='trash-put'
 trash-list
 echo -e "\nuse trash-empty or trash-restore as you see fit"
 rm .bash_history
+#./MuggleBornPadawan/700_linux/bckp/bckp.sh
+# name to be removed in bckup shell script 
+# cp folders to be done
 fortune | cowsay
 # back up - $PATH, init.el?,
 # run hello worlds 
