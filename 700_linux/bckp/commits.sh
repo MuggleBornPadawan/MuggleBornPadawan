b6927786be465254815ec72beb1aa060dfb17b95
MuggleBornPadawan <mugglebornpadawan@icloud.com>
2025-03-23 00:19:11 +0530

    commits

---
+++ 700_linux/bckp/commits.sh
@@ -0,0 +1,23 @@
+echo " - - - "
+echo "test programming blocks"
+cd
+cd MuggleBornPadawan/200_java
+java -jar HelloWorld.jar
+cd
+cd MuggleBornPadawan/300_python
+python3 hello_world.py
+cd
+cd MuggleBornPadawan/100_clisp
+clisp hello-world.lisp
+cd
+cd MuggleBornPadawan/400_r
+Rscript hello_world.R
+cd
+cd MuggleBornPadawan/110_clojure
+clj -M hello_world.clj
+cd
+cd MuggleBornPadawan/120_elisp
+emacs -Q --script hello_world.el
+cd
+echo " - - - "
+neofetch
