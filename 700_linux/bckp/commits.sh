ba05c659fa2de28fd1cef8a7969c4291108e023b
MuggleBornPadawan <mugglebornpadawan@icloud.com>
2025-03-23 18:50:39 +0530

    daily java

--- 700_linux/bckp/commits.sh
+++ 700_linux/bckp/commits.sh
@@ -4,20 +4,44 @@ cd
 cd MuggleBornPadawan/200_java
 java -jar HelloWorld.jar
 cd
+cd MuggleBornPadawan/
+git add .
+git commit -m "daily java"
+cd 
 cd MuggleBornPadawan/300_python
 python3 hello_world.py
 cd
+cd MuggleBornPadawan/
+git add .
+git commit -m "daily python"
+cd 
 cd MuggleBornPadawan/100_clisp
 clisp hello-world.lisp
 cd
+cd MuggleBornPadawan/
+git add .
+git commit -m "daily clisp"
+cd 
 cd MuggleBornPadawan/400_r
 Rscript hello_world.R
 cd
+cd MuggleBornPadawan/
+git add .
+git commit -m "daily r"
+cd 
 cd MuggleBornPadawan/110_clojure
 clj -M hello_world.clj
 cd
+cd MuggleBornPadawan/
+git add .
+git commit -m "daily clj"
+cd 
 cd MuggleBornPadawan/120_elisp
 emacs -Q --script hello_world.el
 cd
+cd MuggleBornPadawan/
+git add .
+git commit -m "daily elisp"
+cd 
 echo " - - - "
 neofetch
