7762fbb0e63cfd66e01c9c8c887ddf910173898b
MuggleBornPadawan <mugglebornpadawan@icloud.com>
2025-03-24 19:21:22 +0530

    daily

--- 700_linux/bckp/init.el
+++ 700_linux/bckp/init.el
@@ -68,7 +68,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(multiple-cursors use-package)))
+ '(package-selected-packages '(geiser multiple-cursors use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -76,7 +76,7 @@
  ;; If there is more than one, they won't work right.
  )
 
-;; enable mit scheme for org babel
+;; enable mit scheme, js, python for org babel
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((scheme . t)))
@@ -84,7 +84,5 @@
 ;; set mit scheme as the default Scheme implementation
 (setq org-babel-scheme-cmd "mit-scheme")
 
-;; enable javascript
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((js . t)))
+;; Don't ask for confirmation when executing code blocks
+;;(setq org-confirm-babel-evaluate nil)
