cf1eea4f13cb82ed3489ba2b942cbbd2d1201c2b
MuggleBornPadawan <mugglebornpadawan@icloud.com>
2025-05-28 23:06:35 +0530

    daily java

--- 130_mit_scheme/hello_world.scm
+++ 130_mit_scheme/hello_world.scm
@@ -1,5 +1,2 @@
-;; my_first_program.scm
-;; This is a comment - Scheme ignores lines starting with ;;
-
 (display "Hello, Scheme World!") ; This prints a string to the console
 (newline) ; This moves the cursor to the next line, like pressing Enter
