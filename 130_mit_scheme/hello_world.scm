9cefc2a9d60be181f04fe1ef4477a469e7747d6c
MuggleBornPadawan <mugglebornpadawan@icloud.com>
2025-05-28 23:10:29 +0530

    daily python

--- 130_mit_scheme/hello_world.scm
+++ 130_mit_scheme/hello_world.scm
@@ -1,2 +1,18 @@
-(display "Hello, Scheme World!") ; This prints a string to the console
-(newline) ; This moves the cursor to the next line, like pressing Enter
+(define current-dt (current-datetime))
+
+(display "Hello, Scheme World! ")
+(display "The current date and time is: ")
+(display (datetime-year current-dt))
+(display "-")
+(display (datetime-month current-dt))
+(display "-")
+(display (datetime-day current-dt))
+(display " ")
+(display (datetime-hour current-dt))
+(display ":")
+(display (datetime-minute current-dt))
+(display ":")
+(display (datetime-second current-dt))
+(newline)
+
+(exit) ; Crucial for shebang scripts to exit cleanly
