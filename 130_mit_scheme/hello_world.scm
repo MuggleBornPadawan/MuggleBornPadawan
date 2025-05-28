da86a7debea6653e2a8e7a0094e62be1f2c86aa8
MuggleBornPadawan <mugglebornpadawan@icloud.com>
2025-05-28 23:00:01 +0530

    daily cpp

--- 130_mit_scheme/hello_world.scm
+++ 130_mit_scheme/hello_world.scm
@@ -1,2 +1,5 @@
-(display "Hello, mit-scheme world!")
-(newline)
+;; my_first_program.scm
+;; This is a comment - Scheme ignores lines starting with ;;
+
+(display "Hello, Scheme World!") ; This prints a string to the console
+(newline) ; This moves the cursor to the next line, like pressing Enter
