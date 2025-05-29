18ff7d04031d21d3d62b07d210494aeea39bf8d0
MuggleBornPadawan <mugglebornpadawan@icloud.com>
2025-05-29 13:15:48 +0530

    daily clisp

--- 150_racket_scheme/hello_world.rkt
+++ 150_racket_scheme/hello_world.rkt
@@ -6,6 +6,6 @@
 ;; Dependencies: Racket standard library
 
 ;; This is the classic "Hello, World!"
-(display "Hello, World!\n")
+(display "Hello, Racket World! Current date and time is: \n")
 (system "date")
-(system "time")
+(newline)
