4733f2dba2a5694a03f17746dd3c99b5f7fd5f20
MuggleBornPadawan <mugglebornpadawan@icloud.com>
2025-05-29 13:06:30 +0530

    daily java

--- 150_racket_scheme/hello_world.rkt
+++ 150_racket_scheme/hello_world.rkt
@@ -1,7 +1,6 @@
 #lang racket
 ;; GNU General Public License v3.0
-;; Copyright (C) 2025 Your Name Here (Replace with your name!)
-;; Author: Your Name Here (Replace with your name!)
+;; Copyright (C) 2025 MuggleBornPadawan
 ;; Description: My first "Hello, World!" program with current date and time in Racket.
 ;; Version: 1.0
 ;; Dependencies: Racket standard library
