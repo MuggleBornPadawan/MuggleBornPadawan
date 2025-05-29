248e6a7b98c2bd93e1640c54d95eff042c82597e
MuggleBornPadawan <mugglebornpadawan@icloud.com>
2025-05-29 15:11:33 +0530

    daily java

--- 700_linux/scripts/gemini_query.sh
+++ 700_linux/scripts/gemini_query.sh
@@ -19,11 +19,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-echo "Did you check pass from cmd?"
-echo "Are you sure you want to continue?"
-echo "(Press Enter to continue or Ctrl+C to abort)"
-read -p ""
-
 MY_GEMINI_API_KEY=$(pass GEMINI_API_KEY)
 
 if [ -z "$MY_GEMINI_API_KEY" ]; then
