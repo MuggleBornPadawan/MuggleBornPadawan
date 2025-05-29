323dc00f6ef34c12555fb3441328e0012fb826b2
MuggleBornPadawan <mugglebornpadawan@icloud.com>
2025-05-29 12:14:11 +0530

    daily python

--- 130_mit_scheme/random-utils.scm
+++ 130_mit_scheme/random-utils.scm
@@ -1,78 +1,41 @@
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program. If not, see <https://www.gnu.org/licenses/>.
-;;
-;; Author: MuggleBornPadawan
-;; Version: 2025-05-27
+;;; GNU General Public License v3.0
+;;;
+;;; Copyright (C) 2025 Your Name
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-(define (square x) (* x x))
-(define (sum-of-squares a b)
-  (+ (square a) (square b)))
-(define (f a)
-  (sum-of-squares (+ a 1) (* a 2)))
-(define (abs x)
-  (cond ((> x 0) x)
-	((= x 0) 0)
-	((< x 0) (- x))))
-(define (random-integer-in-range min-val max-val)
-  (if (> min-val max-val)
-      (error "Min value cannot exceed max value in random-integer-in-range. "
-             "min-val:" min-val " max-val:" max-val)
-      (let ((range-size (+ (- max-val min-val) 1)))
-        (+ min-val (random range-size)))))
-(define (sum-of-squares-of-two-larger x y z)
-  (define (square n) (* n n)) ; Helper function for squaring
-  (max (+ (square x) (square y))
-       (+ (square x) (square z))
-       (+ (square y) (square z))))
-(define (a-plus-abs-b a b)
-  ((if (> b 0) + -) a b))
-(define (p) (p))
-(define (test x y)
-  (if (= x 0) 0 y))
-; sqrt
-(define (sqrt x)
-  (define (average x y)
-    (/ (+ x y) 2))
-  (define (good-enough? guess)
-    (< (abs (- (square guess) x)) 0.001))
-  (define (improve guess)
-    (average guess (/ x guess)))
-  (define (sqrt-iter guess)
-    (display "Current guess: ")
-    (display guess)
-    (newline) ; Add a newline for readability
+;; Author: Your Name
+;; Date: 2025-05-29
+;; Description: A simple Scheme script to display the current date and time.
 
-    (if (good-enough? guess)
-	guess
-	(sqrt-iter (improve guess))))
-    (sqrt-iter 1.0))
-; factorial  
-(define (factorial n)
-  ;; <<< INSERT TRACING HERE <<<
-  (display "--> Entered factorial with n = ")
-  (display n)
-  (newline)
-  ;; >>> TRACING ENDS <<<
+(define (display-current-datetime)
+  "Displays the current date and time."
+  (let* ((current-utc-time (current-time))
+         (local-date-time (time-utc->date current-utc-time)))
+    (display "Current Date and Time: ")
+    (display (date-year local-date-time))
+    (display "-")
+    (display (date-month local-date-time))
+    (display "-")
+    (display (date-day local-date-time))
+    (display " ")
+    (display (date-hour local-date-time))
+    (display ":")
+    (display (date-minute local-date-time))
+    (display ":")
+    (display (date-second local-date-time))
+    (newline))) ; Adds a newline character for cleaner output
 
-  (if (= n 1)
-      1
-      (let ((result (* n (factorial (- n 1))))) ; Use let to capture the recursive result before displaying
-        ;; <<< INSERT TRACING HERE FOR RETURN VALUE <<<
-        (display "<-- Exiting factorial with n = ")
-        (display n)
-        (display ", returning = ")
-        (display result)
-        (newline)
-        ;; >>> TRACING ENDS <<<
-        result))) ; Ensure the result is returned
-
+;; Call the function to display the date and time when the script is run
+(display-current-datetime)
