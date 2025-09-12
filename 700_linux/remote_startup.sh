79818767c42667eea6f823137b199f3b45b6c9ac
MuggleBornPadawan <mugglebornpadawan@icloud.com>
2025-09-13 01:59:55 +0530

    tmp rstr pnt

--- 700_linux/remote_startup.sh
+++ 700_linux/remote_startup.sh
@@ -28,9 +28,11 @@ pass ls
 mv daily_nuggets.txt.enc MuggleBornPadawan/700_linux/bckp/
 
 # fetch chennai weather
-curl wttr.in/Chennai | head -n 7
+curl wttr.in/chennai | head -n 7
+curl wttr.in/chennai | tail -n 2 | head -n 1
 # Fetch Pondicherry weather
-curl wttr.in/Pondicherry | head -n 7
+curl wttr.in/pondicherry | head -n 7
+curl wttr.in/pondicherry | tail -n 1 | head -n 1
 ping -w 12 google.com > tmp.txt
 cat tmp.txt | grep "rtt"
 fortune -a | cowsay
