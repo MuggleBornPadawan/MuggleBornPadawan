9352869b53d7dc8031df5aab371f3c30407893aa
MuggleBornPadawan <mugglebornpadawan@icloud.com>
2025-09-19 08:38:19 +0530

    tmp rstr pnt

--- 700_linux/remote_startup.sh
+++ 700_linux/remote_startup.sh
@@ -4,22 +4,19 @@ cd
 espeak -v en-gb -s 175 -p 50 "roger that"
 echo "run this file for any remote debian server setup, startup and chk backups"
 echo -e "\nDate: $(date) \nOS: $(uname -s) \nKernel: $(uname -r)"
-sudo apt-get update
-sudo apt-get dist-upgrade
+sudo apt-get update && sudo apt-get dist-upgrade
 sudo apt-get install gnuplot nasm ffmpeg lm-sensors sqlite3 mpg123 dnsutils make bats jq cron postfix mailutils pass gnupg nmap htop pv tldr tree ncdu parallel tmux rsync bat fd-find git rig espeak nodejs npm openjdk-17-jdk python3 python3-pip mit-scheme racket clojure emacs magit sbcl clisp r-base build-essential firefox-esr fortune cowsay neofetch trash-cli
-sudo apt-get autoremove 
-sudo apt-get clean
-sudo apt-get autoclean
-npm list -g --depth=0
+sudo apt-get autoremove && sudo apt-get clean && sudo apt-get autoclean
+npm list -g --depth=0 && npm outdated && npm update &&  npm audit fix --force && npm install -g npm-check-updates
+
 cp MuggleBornPadawan/.gitignore $HOME
 cp MuggleBornPadawan/.dockerignore $HOME
 cp MuggleBornPadawan/Dockerfile $HOME
 cp MuggleBornPadawan/Jenkinsfile $HOME
-git config --global user.name "MuggleBornPadawan"
-git config --global user.email "mugglebornpadawan@icloud.com"
+git config --global user.name "MuggleBornPadawan" && git config --global user.email "mugglebornpadawan@icloud.com"
 echo -e "\nrun commits seperately; docker and jenkins startup shell scripts to be created seperately"
 alias rm="trash-put"
-alias all-done="git add . && git commit -m 'wip' && git push"
+
 # trash-list # use only to list the files in the trash bin
 echo -e "\nIMPORTANT: restore bkp, if not done yet. list: tmux, emacs, alias, gitignore, dockerignore, jenkins"
 echo -e "\nuse trash-empty or trash-restore as you see fit"
