a30465cd5eae9d66ff81b6a160501ec6e31aaddf
MuggleBornPadawan <mugglebornpadawan@icloud.com>
2025-12-17 23:25:41 +0530

    wip 1765994035

--- 000_refcards/c/test/hello.sh
+++ 000_refcards/c/test/hello.sh
@@ -11,3 +11,20 @@ echo "Hello Shell World!"
 # xdg-open hello.html
 python3 hello.py
 rm hello_cpp hello
+# assignment 
+declare -a myArray
+myArray+=("Linux")
+myArray+=("is")
+myArray+=("cool!")
+echo ${myArray[@]}
+pwd
+currentTS=$(date +%s)
+yesterdayTS=$(($currentTS - 24 * 60 * 60))
+for file in *
+do
+  if [[ $(date -r "$file" +%s) -gt $yesterdayTS ]]
+  then
+      echo $file # [TASK 11]
+  fi
+
+done
