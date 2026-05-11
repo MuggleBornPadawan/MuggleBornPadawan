70508683aba83b9cf2acfc5f29150931bba9fe57
MuggleBornPadawan <mugglebornpadawan@icloud.com>
2026-05-12 00:17:03 +0530

    tmp rstr pnt

---
+++ 700_linux/scripts/info.sh
@@ -0,0 +1,18 @@
+uname -a
+tty
+pwd
+date
+ulimit -a | grep processes
+ping -c 1 www.google.com
+curl -s https://ipinfo.io/json
+uptime
+hostname -i
+w
+whoami
+who
+id
+ip addr show
+df -h
+df -i
+du -sh
+history
