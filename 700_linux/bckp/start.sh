60789cfca69888a02c926fa4c91e903b38a8670f
MuggleBornPadawan <mugglebornpadawan@icloud.com>
2026-06-25 00:56:09 +0530

    daily commit

--- 700_linux/bckp/start.sh
+++ 700_linux/bckp/start.sh
@@ -2,11 +2,12 @@ cd
 cd MuggleBornPadawan
 git status
 git add .
-git commit -m "tmp rstr pnt"
+git commit -m "daily commit"
 git status
 cd
 ./MuggleBornPadawan/700_linux/bckp/commits.sh
 echo -e "\nBckps - tbd"
+cp .gnupg/gpg-agent.conf MuggleBornPadawan/999_dotfiles/.gpg-agent.conf_bkp
 cp .bashrc MuggleBornPadawan/999_dotfiles/.bashrc_bkp
 cp .bash_aliases MuggleBornPadawan/999_dotfiles/.bash_aliases_bkp
 cp .tmux.conf MuggleBornPadawan/999_dotfiles/.tmux.conf.bkp
@@ -20,16 +21,13 @@ read -p ""
 echo "Continuing..."
 ./MuggleBornPadawan/700_linux/remote_startup.sh | tee - a ./MuggleBornPadawan/700_linux/bckp/shell_log.log
 cd
-cd MuggleBornPadawan
-git add .
-git commit -m "log daily"
-git status
-cd
 # backups 
 # ./MuggleBornPadawan/700_linux/bckp/bckp.sh
 ./MuggleBornPadawan/700_linux/scripts/gpg_protector.sh encrypt daily_nuggets.txt 13
 mv daily_nuggets.txt.enc MuggleBornPadawan/700_linux/bckp/
 echo "Backup log - done"
+./MuggleBornPadawan/700_linux/scripts/yadda_yadda.sh
+cd
 rm daily_nuggets.txt model_answers.log aeo_results_log.txt
 rm startup_log.log - a tmp.txt
-./MuggleBornPadawan/700_linux/scripts/yadda_yadda.sh
+rm tempF*
