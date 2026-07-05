818614e3eb809de6abbb41ff66fb8f8cfeb41d79
MuggleBornPadawan <mugglebornpadawan@icloud.com>
2026-07-05 06:12:20 +0530

    daily mit-scheme

--- 110_clojure/clojure_for_the_brave/clojure-noob/src/clojure_noob/core.clj
+++ 110_clojure/clojure_for_the_brave/clojure-noob/src/clojure_noob/core.clj
@@ -4,6 +4,12 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "I'm a little teapot.. yey!")
-  (do (println "no prompt here!") (+ 1 333 4 7 33))
-)
+  (println "I'm a little teapot.. yeYyyyy!")
+  (do (println "no prompt here!") (+ 1 333 4 7 33)))
+
+(println "Cleanliness is next to godliness")
+
+(defn train
+  "run a train"
+  []
+  (println "Choo choo!"))
--- 999_dotfiles/readme.txt
+++ 999_dotfiles/readme.txt
@@ -1,3 +1,4 @@
+- ~/emacs.d/init.el - move the "customizations" folder (in this folder) to your ~/emacs.d folder for the config file to work
 To be updated
 .bashrc
 .tmux
