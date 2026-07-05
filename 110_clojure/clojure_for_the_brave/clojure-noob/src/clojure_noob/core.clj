ab9f0b5fba19d54f986354c7e6f50f5f86de5a8e
MuggleBornPadawan <mugglebornpadawan@icloud.com>
2026-07-05 13:58:36 +0530

    daily mit-scheme

--- 110_clojure/clojure_for_the_brave/clojure-noob/src/clojure_noob/core.clj
+++ 110_clojure/clojure_for_the_brave/clojure-noob/src/clojure_noob/core.clj
@@ -4,12 +4,12 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "I'm a little teapot.. yeYyyyy!")
+  (println "I'm a little teapot.. yay!")
   (do (println "no prompt here!") (+ 1 333 4 7 33)))
 
-(println "Cleanliness is next to godliness")
+(println "cleanliness is next to godliness")
 
 (defn train
-  "run a train"
+  "run a train well"
   []
   (println "Choo choo!"))
